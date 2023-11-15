61ddc9b099ce6bcd85782345a655c08a2c9194ad
ibbuhussain <87323553+ibbuhussain@users.noreply.github.com>
2023-11-15 12:19:06 +0530

    Organised it with line separations

--- routingNode.js
+++ routingNode.js
@@ -1,58 +1,114 @@
-//TO BE NOTED
-// js files should be in public folder for HTML files to be routed in server.
-
-import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+/*/IMPORTANT NOTES
+In order to route to html from the respective js files,
+ js files should be in public folder
+-only ('/' for GET) , ('/xyz' for post) */
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser";
-import ejs from 'ejs'
+import ejs from 'ejs';
+import path from "path";import { dirname } from "path";import { fileURLToPath } from "url";const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import session from 'express-session';
+import passport from 'passport';
+import flash from 'connect-flash';
 
 const app = express();
-const port = 3000;
-// Set up a template engine (e.g., EJS) if you want to use res.render
+const port = 3005;
 app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 
+// Configure session
+app.use(session({
+  secret: 'aL0nG_R@nd0m_S3cr3t_K3y',
+  resave: false,
+  saveUninitialized: true
+}));
+// Initialize Passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+
+//------------------------  Use middleware------------------------ 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use('/public', express.static(path.join(__dirname, './public')));
  app.use(express.static(path.join(__dirname, '/public')));
 
-//_________________________ required js files ______________________________________________________________________________
+app.use(flash());// Use connect-flash 
+import pinAuthentication from './customizedMiddlewares.js'; // for TODO
+//------------------------ ------------------------ ------------------------ 
+
+//------------------------  Use routes------------------------ 
+import mainLogn from './routes/mainLogin.js';
+import directions from './routes/nav.js';
+import secureit from './routes/securePages.js';
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+
+//------------------------ required js files ---------------------------------------------------------------------------------
 //const db = require("./jsLogic/db.js"); // Import the database connection in CJS
  import db from "./public/jsLogic/database.js"; // Import the database connection in ES
-//____________REMEMBER__export is required in respective js files______________________________________________________________
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 
-//__________________________BELOW ARE GET HANDLES__________________________________________________________
-app.get("/", (req, res) => {
-    // Use res.sendFile to send an HTML file
-    res.sendFile(path.join(__dirname, 'views', 'login.html'));
-   //For using EJS
-    res.render('login.ejs', { user }); //login is relative to views folder by default
+
+//-----------------------------------------------------------------------------------------------------------------------------
+//                          ACTUAL ROUTING BEGINS
+//-----------------------------------------------------------------------------------------------------------------------------
+app.use('/',directions); //All get requests are here handled
+app.use('/login', mainLogn); //from PP to dashbrd // handling POST request
+app.use('/',secureit); //FOr Security
+app.use('/', mainLogn); //from PP to dashbrd for LOGOUT -only'/' for GET , /xyz for post
+
+//------------------------ ------------------------ ------------------------- 
+//-------------  todoDisplay  HERE -------------  ------------- ------------- 
+//------------------------ ------------------------ ------------------------- 
+app.get("/todo", async (req, res) => {
+    //res.sendFile(path.join(__dirname, 'views', 'todoView.html'));
+    res.render('todoView.ejs');
 });
+app.get('/api/todos', async (req, res) => {
+    try {
+        let todo = [];
+        const result = await db.query("SELECT * FROM crtable ORDER BY id DESC");
+        todo = result.rows;
+        //console.log("SELECTQUERY");
+        res.json(todo);
 
+    } catch (err) {
+        console.log(err);
+    }
+});
+//------------------------ ------------------------ ------------------------- 
+//-------- Route for PIN authentication via MiddleWare--------- ------------- 
+//------------------------ ------------------------ ------------------------- 
+app.post('/authenticate-pin', pinAuthentication, (req, res) => {
+  // Redirect to the protected page with authentication query parameter
+  //res.redirect('/todo');
+  res.redirect('/todo?modalNotDisplayed=true');
+});
+//------------------------ ------------------------ ------------------------- 
+//--------Handle POST requests to /addTask  HERE--------------- ------------- 
+//------------------------ ------------------------ ------------------------- 
+app.post('/addTask', async (req, res) => {
+    const { id, name, status, date } = req.body;
+  
+    try {
+      // Perform the database update logic for adding a new task
+      await db.query("INSERT INTO crtable (name, status, date) VALUES ($1, $2, $3)", [name, status, date]);
+  
+      // Redirect after sending the JSON response
+      return res.redirect("/todo?modalNotDisplayed=true");
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
-//__________________________BELOW ARE POST HANDLES__________________________________________________________
-
-// Define a route to handle login
-app.post('/login', (req, res) => {
-  const { U_name, password } = req.body;
- //Login check via Dummy user data stored in an array
-const users = [  { username: "xyz", password: "123" }  ];
-    // Check if the entered credentials match a user in the array
-    const user = users.find(u => u.username === U_name && u.password === password);
-    if (user) {  // Login successful, store user in local storage
-        res.redirect("HOme");
-    } 
-    else {   // Login failed
-      res.render('PP', { error: 'Invalid Username or Password' }); // Use EJS file to display the error
-      }     
-}); //app.post close bracket
-
-//\\\\\\\\\\\\\\\\\\\\\\\\ L I S T E N   R O U T E \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+////¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬ E N D ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+//¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
